227e749b15c26b22ad69f3259d78bbba2a1082ed
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2018-06-22 22:00:21 -0500

    add a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Headquarter.create(name: 'Salud Total EPS', code: 7600104096)
+Headquarter.create(name: 'Virrey Solis IPS', code: 7600110304)
+
+branch_offices = [
+	[ "Americas", 1, 1],
+	[ "Manzanares", 1, 2],
+	[ "San Fernando", 2, 2],
+	[ "San Fernando UOD", 3, 2],
+	[ "Versalles", 4, 2],
+	[ "Oriente", 5, 2]
+]
+
+branch_offices.each do |name, code, headquarter_id|
+	BranchOffice.create(name: name, code: code, headquarter_id: headquarter_id)
+end
