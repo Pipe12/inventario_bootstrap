db9ac7c1fff40ba6326537b96eacf9aa4f72515e
Andres Burbano <ciento1@hotmail.com>
2018-06-22 21:20:10 -0500

    work in a show headquarter view

--- app/controllers/headquarters_controller.rb
+++ app/controllers/headquarters_controller.rb
@@ -4,6 +4,12 @@ class HeadquartersController < ApplicationController
   end
 
   def show
-    
+    @headquarter = Headquarter.find(params[:id])
+    @branchOffices  = BranchOffice.where(headquarter_id: @headquarter.id)
   end
+
+  private
+    def headquarter_params
+      params.require(:headquarter).permit(:name, :code)
+    end
 end
