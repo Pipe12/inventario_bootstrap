3e716b4dbfed7d64af15d3c3b26c97ec3d8bc2c4
Andres Burbano <ciento1@hotmail.com>
2018-07-06 09:13:43 -0500

    send locations to branch offices

--- app/controllers/branch_offices_controller.rb
+++ app/controllers/branch_offices_controller.rb
@@ -5,6 +5,7 @@ class BranchOfficesController < ApplicationController
   def show
     @branchoffice = BranchOffice.find(params[:id])
     @headquarter = Headquarter.find(@branchoffice.headquarter_id)
+    @locations = Location.where(branch_office_id: @branchoffice.id)
   end
 
   private
--- app/models/branch_office.rb
+++ app/models/branch_office.rb
@@ -1,3 +1,4 @@
 class BranchOffice < ApplicationRecord
   belongs_to :headquarter
+  has_many :locations
 end
